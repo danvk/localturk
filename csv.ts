98afd0e0caa4863622ef518cf1f34c64df9c5839
Dan Vanderkam <danvdk@gmail.com>
2017-12-03 18:07:13 -0500

    readRows

--- csv.ts
+++ csv.ts
@@ -5,12 +5,16 @@ if (!(Symbol as any)['asyncIterator']) {
   (Symbol as any)['asyncIterator'] = Symbol();
 }
 
+const csvOptions: csvParse.Options = {
+  skip_empty_lines: true
+};
+
 /** Read just the headers from a CSV file. */
 export async function readHeaders(file: string) {
-  const parser = csvParse({skip_empty_lines: true});
+  const parser = csvParse(csvOptions);
   const stream = fs.createReadStream(file, 'utf8');
   return new Promise((resolve, reject) => {
-    parser.on('data', (row) => {
+    parser.on('data', (row: string[]) => {
       resolve(row);
       parser.pause();
       stream.destroy();
@@ -20,9 +24,70 @@ export async function readHeaders(file: string) {
   });
 }
 
+interface Row {
+  type: 'row';
+  value: string[];
+}
+interface Error {
+  type: 'error';
+  error: any;
+}
+interface Done {
+  type: 'done';
+}
+type RowResult = Row | Error | Done;
+
+function isPromise(x: any): x is Promise<any> {
+  return ('then' in x);
+}
+
 /** Read a CSV file line-by-line. */
 export async function* readRows(file: string) {
+  const parser = csvParse(csvOptions);
+  const stream = fs.createReadStream(file, 'utf8');
 
+  let isDone = false;
+  let dataCallback;
+  const mkBarrier = () => new Promise<void>((resolve, reject) => {
+    dataCallback = resolve;
+  });
+
+  // TODO(danvk): use a deque
+  const rows: (RowResult|Promise<void>)[] = [mkBarrier()];
+  parser.on('readable', () => {
+    let row;
+    while (row = parser.read()) {
+      rows.push({type: 'row', value: row});
+    }
+    const oldCb = dataCallback;
+    rows.push(mkBarrier());
+    oldCb();
+  });
+  parser.on('error', error => {
+    rows.push({type: 'error', error});
+    parser.pause();
+    dataCallback();
+  });
+  parser.on('finish', () => {
+    rows.push({type: 'done'});
+    dataCallback();
+  });
+  stream.pipe(parser);
+
+  while (rows.length) {
+    const row = rows.shift();
+    if (isPromise(row)) {
+      await row;
+    } else {
+      if (row.type === 'row') {
+        yield row.value;
+      } else if (row.type === 'error') {
+        throw new Error(row.error);
+      } else if (row.type === 'done') {
+        break;
+      }
+    }
+  }
 }
 
 /**
--- test/csv_test.ts
+++ test/csv_test.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs-extra';
+
 import * as csv from '../csv';
 
 import {expect} from 'chai';
@@ -9,6 +11,39 @@ describe('csv', () => {
       'id',
       'First',
       'Last'
+    ]);
+  });
+
+  it('should read quoted CSV headers', async () => {
+    const headers = await csv.readHeaders('./test/quoted.csv');
+    expect(headers).to.deep.equal([
+      'id',
+      'First Name',
+      'Last,Name'
+    ]);
+  });
+
+  it('should read lines in a simple CSV file', async () => {
+    const rows = [];
+    for await (const row of csv.readRows('./test/test.csv')) {
+      rows.push(row);
+    }
+    expect(rows).to.deep.equal([
+      ['id', 'First', 'Last'],
+      ['1', 'Jane', 'Doe'],
+      ['2', 'John', 'Doer'],
+    ]);
+  });
+
+  it('should read lines in a complex CSV file', async () => {
+    const rows = [];
+    for await (const row of csv.readRows('./test/quoted.csv')) {
+      rows.push(row);
+    }
+    expect(rows).to.deep.equal([
+      ['id', 'First Name', 'Last,Name'],
+      ['1', 'Jane\nDoe', 'Doe'],
+      ['2', 'John', 'Doer\nQuoter'],
     ]);
   });
 });
